da4a2c1cc5646589e834b28ab83fba3a851e9b03
André Capitão <andre.f.g.capitao@gmail.com>
2026-03-10 19:07:40 +0000

    Update base page

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/home-paws-vet">
         <Routes>
           <Route path="/" element={<Index />} />
           
